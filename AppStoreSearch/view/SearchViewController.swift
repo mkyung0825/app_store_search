9f9fd5e9abf273e5b92deff1b767536b70dd2922
mikyung <mkyung0825@gmail.com>
2020-07-25 13:05:48 +0900

    - 최근 검색어, 필터 목록, 검색 결과 테이블뷰 섹션 분리

--- AppStoreSearch/view/SearchViewController.swift
+++ AppStoreSearch/view/SearchViewController.swift
@@ -7,6 +7,13 @@
 
 import UIKit
 
+enum SEARCH_SECTION:Int {
+    case RECENT_SEARCH = 0
+    case FILTER
+    case SEARCH_RESULT
+    case NONE
+}
+
 class SearchViewController: UIViewController {
     
     @IBOutlet weak var mTableView: UITableView!
@@ -15,6 +22,9 @@ class SearchViewController: UIViewController {
     
     // 필터 적용 여부
     var mIsFiltering:Bool = false
+    
+    // 검색 결과 노출 여부
+    var mIsShowResult:Bool = false
     
     // 로컬에 저장 된 최근 검색어 목록
     var mRecentSearchList:[String] = []
@@ -70,6 +80,7 @@ class SearchViewController: UIViewController {
         mTableView.reloadData()
     }
     
+    // 최근 검색어 필터
     func filterRecentSearch(text: String) {
         
         let searchList = Common.getRecentSearchList()
@@ -83,6 +94,63 @@ class SearchViewController: UIViewController {
 
         setTableViewData(recentSearchList: filterList)
     }
+    
+    // 검색어 클릭
+    func clickSearchItem(text: String) {
+        LOG("click : \(text)")
+    }
+    
+    
+    
+    
+    // MARK:- setting section
+    func recentSearchSection() -> Int {
+        var sction:Int = -1
+        if !mIsFiltering && !mIsShowResult {
+            sction += 1
+        }
+        return sction
+    }
+    func filterSection() -> Int {
+        var sction:Int = recentSearchSection()
+        if mIsFiltering && !mIsShowResult {
+            sction += 1
+        }
+        return sction
+    }
+    func searchResultSection() -> Int {
+        var sction:Int = filterSection()
+        if !mIsFiltering && mIsShowResult {
+            sction += 1
+        }
+        return sction
+    }
+    func maxSection() -> Int {
+        return searchResultSection() + 1
+    }
+    
+    func getSectionType(section: Int) -> SEARCH_SECTION {
+        var secType:SEARCH_SECTION = SEARCH_SECTION.init(rawValue: section) ?? .NONE
+        
+        switch section {
+        case recentSearchSection():
+            secType = .RECENT_SEARCH
+            
+        case filterSection():
+            secType = .FILTER
+            
+        case searchResultSection():
+            secType = .SEARCH_RESULT
+            
+        case maxSection():
+            secType = .NONE
+            
+        default:
+            break
+        }
+        
+        return secType
+    }
 }
 
 
@@ -90,6 +158,11 @@ class SearchViewController: UIViewController {
 
 // MARK: - extension UITableView
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    // section
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return maxSection()
+    }
+    
     // row
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return mRecentSearchList.count
@@ -97,25 +170,51 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
     // cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let cell:RecentSearchCell = tableView.dequeueReusableCell(withIdentifier: "RecentSearchCell") as! RecentSearchCell
-        cell.setData(text: mRecentSearchList[indexPath.row])
-        return cell
+        let sectionType:SEARCH_SECTION = getSectionType(section: indexPath.section)
+        
+        if sectionType == .RECENT_SEARCH {
+            let cell:RecentSearchCell = tableView.dequeueReusableCell(withIdentifier: "RecentSearchCell") as! RecentSearchCell
+            cell.setData(text: mRecentSearchList[indexPath.row])
+            return cell
+            
+        } else if sectionType == .FILTER {
+            let cell:RecentSearchCell = tableView.dequeueReusableCell(withIdentifier: "RecentSearchCell") as! RecentSearchCell
+            cell.setData(text: mRecentSearchList[indexPath.row])
+            return cell
+            
+        } else if sectionType == .SEARCH_RESULT {
+            
+        }
+        return UITableViewCell()
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 40
+        let sectionType:SEARCH_SECTION = getSectionType(section: indexPath.section)
+        
+        if sectionType == .RECENT_SEARCH
+            || sectionType == .FILTER {
+            return 40
+            
+        } else if sectionType == .SEARCH_RESULT {
+            
+        }
+        return 0
     }
     
     // header
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        if !mIsFiltering {
+        let sectionType:SEARCH_SECTION = getSectionType(section: section)
+        
+        if sectionType == .RECENT_SEARCH {
             let view:RecentSearchHeaderView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "RecentSearchHeaderView") as! RecentSearchHeaderView
             return view
         }
         return nil
     }
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        if !mIsFiltering {
+        let sectionType:SEARCH_SECTION = getSectionType(section: section)
+        
+        if sectionType == .RECENT_SEARCH {
             return 60
         }
         return 0
@@ -123,7 +222,17 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
     // select
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-     
+        let sectionType:SEARCH_SECTION = getSectionType(section: indexPath.section)
+
+        if sectionType == .RECENT_SEARCH {
+            clickSearchItem(text: mRecentSearchList[indexPath.row])
+
+        } else if sectionType == .FILTER {
+            clickSearchItem(text: mRecentSearchList[indexPath.row])
+            
+        } else if sectionType == .SEARCH_RESULT {
+
+        }
     }
 }
 
@@ -171,6 +280,9 @@ extension SearchViewController: UISearchBarDelegate {
         Common.setRecentSearchList(value: searchList)
         
         LOG("->\(Common.getRecentSearchList())")
+        
+        // 검색하기
+        clickSearchItem(text: searchText)
     }
     
     // cancel
